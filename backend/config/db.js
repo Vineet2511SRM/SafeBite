d7b947ea3ce15e56bdc138d36dfe48b5dc4c0cd7
VINEET2511SRM <emperorvineet7@gmail.com>
2026-04-24 21:59:37 +0530

    initialize database connection configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const db = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'vineet12@11',
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'SafeBite',
     waitForConnections: true,
     connectionLimit: 10,
